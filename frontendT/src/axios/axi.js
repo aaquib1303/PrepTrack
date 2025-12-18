7cf5fc0675fc1ca03d7329c0521f235705741197
aaquib1303 <aaquibuddin.md@gmail.com>
2025-12-18 20:15:40 +0530

    Updated axi.js for deployment

--- frontendT/src/axios/axi.js
+++ frontendT/src/axios/axi.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api", 
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000", 
 });
 
 API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = `Bearer ${token}`;
+  if (localStorage.getItem("token")) {
+    req.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+  }
   return req;
 });
 
